dd5be8fb83eacd194ece05b081305afade396710
thisguyEly <thisguyely@gmail.com>
2026-03-15 21:21:07 -0400

    added basic starter R methods

---
+++ ViewStyleData.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(datasets)
+library(dplyr)
+
+
+# displays the column names
+names(iris)
+
+# shows the first 6 rows
+#head()
+colnames()
+# shows the last 6 rows
+tail(iris)
+
+#iris |>
+ # filter(Sepal.Length > 5.0) |>
+  #View()
+
